9339023c579be7f89e89c2965dd0b7522f6dcaa7
Fabian Wennink <contact@fabianwennink.nl>
2018-10-27 22:36:14 +0200

    Finished testing the services.
    - Tested both the playlist and track service. Although the tests are all kind of pointless, it might prevent possible future errors when business logic is introduced.

--- src/test/java/nl/fabianwennink/dea/services/PlaylistServiceTest.java
+++ src/test/java/nl/fabianwennink/dea/services/PlaylistServiceTest.java
@@ -1,4 +1,141 @@
 package nl.fabianwennink.dea.services;
 
-public class PlaylistServiceTest {
+import nl.fabianwennink.dea.TestConstants;
+import nl.fabianwennink.dea.controllers.playlist.dto.PlaylistDTO;
+import nl.fabianwennink.dea.database.dao.PlaylistDAO;
+import nl.fabianwennink.dea.database.entities.Playlist;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlaylistServiceTest extends TestConstants {
+
+    @Mock private PlaylistDAO playlistDAO;
+    private PlaylistService playlistService;
+
+    private List<Playlist> playlistEntityList = new ArrayList<>();
+
+    private static final String NEW_PLAYLIST_TITLE = "New Playlist Name";
+    private static final int TOTAL_PLAYLIST_DURATION = 12345;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        playlistService = new PlaylistService();
+        playlistService.setPlaylistDAO(playlistDAO);
+
+        // Fill the playlistEntityList list with dummy entities.
+        playlistEntityList.add(new Playlist());
+        playlistEntityList.add(new Playlist());
+    }
+
+    @Test
+    public void should_ReturnAllPlaylists() {
+        Mockito.when(playlistDAO.getAll()).thenReturn(playlistEntityList);
+
+        // Test currently somewhat pointless since the service is missing any business logic.
+        // Still tested in case of possible future changes in the method (addition of logic).
+
+        List<PlaylistDTO> playlists = playlistService.getAll(AUTHENTICATED_USER_ID);
+
+        Assertions.assertNotNull(playlists);
+        Assertions.assertTrue(playlists.size() > 0);
+    }
+
+    @Test
+    public void should_ReturnTrue_IfPlaylistWasEdited() {
+        Mockito.when(playlistDAO.editTitle(VALID_PLAYLIST_ID, NEW_PLAYLIST_TITLE, AUTHENTICATED_USER_ID)).thenReturn(true);
+
+        // Test currently somewhat pointless since the service is missing any business logic.
+        // Still tested in case of possible future changes in the method (addition of logic).
+
+        Assertions.assertTrue(playlistService.editTitle(VALID_PLAYLIST_ID, NEW_PLAYLIST_TITLE, AUTHENTICATED_USER_ID));
+    }
+
+    @Test
+    public void should_ReturnFalse_IfPlaylistWasNotEdited() {
+        Mockito.when(playlistDAO.editTitle(INVALID_PLAYLIST_ID, NEW_PLAYLIST_TITLE, AUTHENTICATED_USER_ID)).thenReturn(false);
+
+        // Test currently somewhat pointless since the service is missing any business logic.
+        // Still tested in case of possible future changes in the method (addition of logic).
+
+        Assertions.assertFalse(playlistService.editTitle(INVALID_PLAYLIST_ID, NEW_PLAYLIST_TITLE, AUTHENTICATED_USER_ID));
+    }
+
+    @Test
+    public void should_ReturnTrue_IfPlaylistWasAdded() {
+        Mockito.when(playlistDAO.create(Mockito.any(Playlist.class))).thenReturn(true);
+
+        // Test currently somewhat pointless since the service is missing any business logic.
+        // Still tested in case of possible future changes in the method (addition of logic).
+
+        Assertions.assertTrue(playlistService.add(new PlaylistDTO(), AUTHENTICATED_USER_ID));
+    }
+
+    @Test
+    public void should_ReturnFalse_IfPlaylistWasAdded() {
+        Mockito.when(playlistDAO.create(Mockito.any(Playlist.class))).thenReturn(false);
+
+        // Test currently somewhat pointless since the service is missing any business logic.
+        // Still tested in case of possible future changes in the method (addition of logic).
+
+        Assertions.assertFalse(playlistService.add(new PlaylistDTO(), AUTHENTICATED_USER_ID));
+    }
+
+    @Test
+    public void should_ReturnTrue_IfPlaylistWasDeleted() {
+        Mockito.when(playlistDAO.delete(Mockito.anyInt(), Mockito.anyInt())).thenReturn(true);
+
+        // Test currently somewhat pointless since the service is missing any business logic.
+        // Still tested in case of possible future changes in the method (addition of logic).
+
+        Assertions.assertTrue(playlistService.delete(VALID_PLAYLIST_ID, AUTHENTICATED_USER_ID));
+    }
+
+    @Test
+    public void should_ReturnFalse_IfPlaylistWasNotDeleted() {
+        Mockito.when(playlistDAO.delete(Mockito.anyInt(), Mockito.anyInt())).thenReturn(false);
+
+        // Test currently somewhat pointless since the service is missing any business logic.
+        // Still tested in case of possible future changes in the method (addition of logic).
+
+        Assertions.assertFalse(playlistService.delete(VALID_PLAYLIST_ID, AUTHENTICATED_USER_ID));
+    }
+
+    @Test
+    public void should_ReturnTotalDurationOfPlaylists() {
+        Mockito.when(playlistDAO.getTotalDuration()).thenReturn(TOTAL_PLAYLIST_DURATION);
+
+        // Test currently somewhat pointless since the service is missing any business logic.
+        // Still tested in case of possible future changes in the method (addition of logic).
+
+        Assertions.assertEquals(TOTAL_PLAYLIST_DURATION, playlistService.getTotalDuration());
+    }
+
+    @Test
+    public void should_ReturnTrue_IfPlaylistOwnedByUser() {
+        Mockito.when(playlistDAO.isOwnedByUser(Mockito.eq(AUTHENTICATED_USER_ID), Mockito.anyInt())).thenReturn(true);
+
+        // Test currently somewhat pointless since the service is missing any business logic.
+        // Still tested in case of possible future changes in the method (addition of logic).
+
+        Assertions.assertTrue(playlistService.isOwnedByUser(AUTHENTICATED_USER_ID, VALID_PLAYLIST_ID));
+    }
+
+    @Test
+    public void should_ReturnFalse_IfPlaylistNotOwnedByUser() {
+        Mockito.when(playlistDAO.isOwnedByUser(Mockito.eq(AUTHENTICATED_USER_ID), Mockito.anyInt())).thenReturn(false);
+
+        // Test currently somewhat pointless since the service is missing any business logic.
+        // Still tested in case of possible future changes in the method (addition of logic).
+
+        Assertions.assertFalse(playlistService.isOwnedByUser(AUTHENTICATED_USER_ID, VALID_PLAYLIST_ID));
+    }
 }
--- src/test/java/nl/fabianwennink/dea/services/TrackServiceTest.java
+++ src/test/java/nl/fabianwennink/dea/services/TrackServiceTest.java
@@ -1,16 +1,27 @@
 package nl.fabianwennink.dea.services;
 
+import nl.fabianwennink.dea.TestConstants;
+import nl.fabianwennink.dea.controllers.tracks.dto.TrackDTO;
 import nl.fabianwennink.dea.database.dao.TrackDAO;
+import nl.fabianwennink.dea.database.entities.Track;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
-public class TrackServiceTest {
+import java.util.ArrayList;
+import java.util.List;
+
+public class TrackServiceTest extends TestConstants {
 
     @Mock private TrackDAO trackDAO;
     private TrackService trackService;
+
+    private List<Track> trackEntityList = new ArrayList<>();
 
     @BeforeEach
     public void setUp() {
@@ -18,29 +29,75 @@ public class TrackServiceTest {
 
         trackService = new TrackService();
         trackService.setTrackDAO(trackDAO);
+
+        // Fill the trackEntityList list with dummy entities.
+        trackEntityList.add(new Track());
+        trackEntityList.add(new Track());
     }
 
     @Test // TrackService.getAllByPlaylistId
-    @Disabled
     public void should_ReturnAllTracks_ByPlaylistID() {
+        Mockito.when(trackDAO.getAllInPlaylist(VALID_PLAYLIST_ID)).thenReturn(trackEntityList);
 
+        List<TrackDTO> tracks = trackService.getAllByPlaylistId(VALID_PLAYLIST_ID);
+
+        // Test currently pointless since the service is missing any business logic.
+        // Still tested in case of possible future changes in the method (addition of logic).
+
+        Assertions.assertNotNull(tracks);
+        Assertions.assertTrue(tracks.size() > 0);
     }
 
     @Test // TrackService.getAllNotInPlaylist
-    @Disabled
     public void should_ReturnAllTracksNotInPlaylist_ByPlaylistID() {
+        Mockito.when(trackDAO.getAllNotInPlaylist(VALID_PLAYLIST_ID)).thenReturn(trackEntityList);
 
+        List<TrackDTO> tracks = trackService.getAllNotInPlaylist(VALID_PLAYLIST_ID);
+
+        // Test currently somewhat pointless since the service is missing any business logic.
+        // Still tested in case of possible future changes in the method (addition of logic).
+
+        Assertions.assertNotNull(tracks);
+        Assertions.assertTrue(tracks.size() > 0);
     }
 
     @Test // TrackService.addTrackToPlaylist
-    @Disabled
-    public void should_ReturnAllTracks_IfTrackAdded() {
+    public void should_ReturnTrue_IfTrackWasAdded() {
+        Mockito.when(trackDAO.addToPlaylist(Mockito.any(Track.class), Mockito.anyInt())).thenReturn(true);
 
+        // Test currently somewhat pointless since the service is missing any business logic.
+        // Still tested in case of possible future changes in the method (addition of logic).
+
+        Assertions.assertTrue(trackService.addTrackToPlaylist(new TrackDTO(), VALID_PLAYLIST_ID));
+    }
+
+    @Test // TrackService.addTrackToPlaylist
+    public void should_ReturnFalse_IfTrackWasNotAdded() {
+        Mockito.when(trackDAO.addToPlaylist(Mockito.any(Track.class), Mockito.anyInt())).thenReturn(false);
+
+        // Test currently somewhat pointless since the service is missing any business logic.
+        // Still tested in case of possible future changes in the method (addition of logic).
+
+        Assertions.assertFalse(trackService.addTrackToPlaylist(new TrackDTO(), INVALID_PLAYLIST_ID));
     }
 
     @Test // TrackService.deleteFromPlaylist
-    @Disabled
-    public void should_ReturnAllTracks_IfTrackDeleted() {
+    public void should_ReturnTrue_IfTrackWasDeleted() {
+        Mockito.when(trackDAO.deleteFromPlaylist(VALID_PLAYLIST_ID, VALID_TRACK_ID, AUTHENTICATED_USER_ID)).thenReturn(true);
 
+        // Test currently somewhat pointless since the service is missing any business logic.
+        // Still tested in case of possible future changes in the method (addition of logic).
+
+        Assertions.assertTrue(trackService.deleteFromPlaylist(VALID_PLAYLIST_ID, VALID_TRACK_ID, AUTHENTICATED_USER_ID));
+    }
+
+    @Test // TrackService.deleteFromPlaylist
+    public void should_ReturnFalse_IfTrackWasNotDeleted() {
+        Mockito.when(trackDAO.deleteFromPlaylist(INVALID_PLAYLIST_ID, INVALID_TRACK_ID, AUTHENTICATED_USER_ID)).thenReturn(false);
+
+        // Test currently somewhat pointless since the service is missing any business logic.
+        // Still tested in case of possible future changes in the method (addition of logic).
+
+        Assertions.assertFalse(trackService.deleteFromPlaylist(INVALID_PLAYLIST_ID, INVALID_TRACK_ID, AUTHENTICATED_USER_ID));
     }
 }
